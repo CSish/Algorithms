31f916114723e9df4e170e4cb8c436a1327adff6
tactnician <tactnician@gmail.com>
2019-12-12 14:51:27 -0500

    recipe_batches tested working

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,7 +3,19 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  batches = []
+  if not set(ingredients).issuperset(set(recipe)):
+    return 0
+  else:
+    for k in set(recipe) & set(ingredients):
+      print( ingredients[k], recipe[k] )
+      print(int(ingredients[k] /recipe[k] ))
+
+      batches.append( int(ingredients[k] /recipe[k] ) )
+  
+  batches.sort()
+  print(batches)
+  return batches[0]
 
 
 if __name__ == '__main__':
@@ -12,3 +24,10 @@ if __name__ == '__main__':
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+
+
+  # Plan 
+
+  # Loop through recipes 
+  # check for matching keys in a ingredients
+  # if mod recipe item 
